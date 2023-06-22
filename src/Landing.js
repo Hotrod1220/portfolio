4a7491f971db8bb913e8a52d0d143a8867d01e3b
TyrellMartens <tyrell.martens@hotmail.com>
2023-06-22 11:06:01 -0600

    Data added, beginning of the website created.

---
+++ src/Landing.js
@@ -0,0 +1,11 @@
+function Landing() {
+    return (
+        <section>
+            <div>
+                <h1>Tyrell Martens</h1>
+            </div>
+        </section>
+    );
+}
+
+export default Landing;
